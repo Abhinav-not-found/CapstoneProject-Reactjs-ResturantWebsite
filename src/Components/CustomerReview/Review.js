43e74a0ad3acc104a54957ee3a315702af2a2ec6
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-07-11 12:15:17 +0530

    added review and menu section

--- src/Components/CustomerReview/Review.js
+++ src/Components/CustomerReview/Review.js
@@ -1,9 +1,39 @@
 import React from 'react'
-
+import profile1 from "./profile-1.jpeg"
+import profile2 from "./profile-3.jpeg"
+// import profile3 from "./profile-4.jpeg"
+import profile6 from "./profile-6.jpeg"
+import "./Review.css"
 export default function Review() {
   return (
     <div>
-        <h1>Customer Review</h1>
+        <div className='Wrapper'>
+          <div className='Card'>
+             <img src={profile1}></img>
+            <div>
+            <p>"Delicious pizza, friendly staff, quick service. I'll definitely be back!"</p>
+             <h2>John Doe</h2>
+            </div>
+          </div>
+
+          <div className='Card'>
+             <img id='img2' src={profile2}></img>
+            <div>
+            <p>"Mouthwatering pizzas, excellent service, highly recommended for pizza lovers!"</p>
+             <h2>Sophia Anderson</h2>
+            </div>
+          </div>
+          
+          <div className='Card'>
+             <img id='img3' src={profile6}></img>
+            <div>
+            <p>"Delicious flavors, prompt delivery, satisfied cravings. Highly recommend this pizza place!"</p>
+             <h2>Ethan Thompson</h2>
+            </div>
+          </div>
+
+
+        </div>
     </div>
   )
 }
--- src/Components/Dishes/Dishes.js
+++ src/Components/Dishes/Dishes.js
@@ -1,9 +1,126 @@
 import React from 'react'
+import './Dishes.css'
+import pizza_type_1 from "./Images/pizza_type_1.jpeg"
+import pizza_type_2 from "./Images/pizza_type_2.jpeg"
+import pizza_type_3 from "./Images/pizza_type_3.jpeg"
+import pizza_type_4 from "./Images/pizza_type_4.jpeg"
+import pizza_type_5 from "./Images/pizza_type_5.jpeg"
+import pizza_type_6 from "./Images/pizza_type_6.jpeg"
+import pizza_type_7 from "./Images/pizza_type_7.jpeg"
+import pizza_type_8 from "./Images/pizza_type_8.jpeg"
+import pizza_type_9 from "./Images/pizza_type_9.jpeg"
+import pizza_type_10 from "./Images/pizza_type_10.jpeg"
+import pizza_type_11 from "./Images/pizza_type_11.jpeg"
+import pizza_type_12 from "./Images/pizza_type_12.jpeg"
+import pizza_type_13 from "./Images/pizza_type_13.jpeg"
+import pizza_type_14 from "./Images/pizza_type_14.jpeg"
+import pizza_type_15 from "./Images/pizza_type_15.jpeg"
 
 export default function Dishes() {
   return (
     <div>
-        <h1>Dishes</h1>
+      <div id='outer'>
+        <div id='wrapper1'>
+          <h1>Country Special Pizza</h1>
+          <div id='cards'>
+                <div id='card'>
+                  <img src={pizza_type_1}></img>
+                  <h3>Italian<br/> Pizza</h3>
+                  <p>Rs.699</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_2}></img>
+                  <h3>Greek<br/> Pizza</h3>
+                  <p>Rs.699</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_3}></img>
+                  <h3>Sicilian<br/> Pizza</h3>
+                  <p>Rs.699</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_4}></img>
+                  <h3>Roman<br/> Pizza</h3>
+                  <p>Rs.699</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_13}></img>
+                  <h3>Indian<br/> Pizza</h3>
+                  <p>Rs.699</p>
+                </div>
+          </div>
+        </div>
+
+
+
+        <div id='wrapper2'>
+          <h1>Basic Pizza</h1>
+          <div id='cards'>
+                <div id='card'>
+                  <img src={pizza_type_14}></img>
+                  <h3>Cheese<br/> Pizza</h3>
+                  <p>Rs.99</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_5}></img>
+                  <h3>Onion<br/> Pizza</h3>
+                  <p>Rs.99</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_6}></img>
+                  <h3>Tomato<br/> Pizza</h3>
+                  <p>Rs.99</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_7}></img>
+                  <h3>Capsicum<br/> Pizza</h3>
+                  <p>Rs.99</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_8}></img>
+                  <h3>Corn<br/> Pizza</h3>
+                  <p>Rs.99</p>
+                </div>
+          </div>
+        </div>
+
+
+        <div id='wrapper3'>
+          <h1>Vegetarian Pizza</h1>
+          <div id='cards'>
+                <div id='card'>
+                  <img src={pizza_type_15}></img>
+                  <h3>Dblcheese<br/> Pizza</h3>
+                  <p>Rs.199</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_9}></img>
+                  <h3>Vegloaded<br/> Pizza</h3>
+                  <p>Rs.199</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_10}></img>
+                  <h3>Mushroom<br/> Pizza</h3>
+                  <p>Rs.199</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_11}></img>
+                  <h3>Paneer<br/> Pizza</h3>
+                  <p>Rs.199</p>
+                </div>
+                <div id='card'>
+                  <img src={pizza_type_12}></img>
+                  <h3>Olive<br/> Pizza</h3>
+                  <p>Rs.199</p>
+                </div>
+          </div>
+        </div>
+
+
+
+
+
+      </div>
     </div>
   )
 }
