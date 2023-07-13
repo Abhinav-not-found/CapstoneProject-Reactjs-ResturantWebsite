7a66022102af81d6fe747f5ea37d2e4f2a229990
Abhinav-not-found <abhinavisgreat18@gmail.com>
2023-07-13 07:39:17 +0530

    about page updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import "./About.css"
 export default function About() {
   return (
     <div>
-        <h1>About page</h1>
+      <div id='wrapper1'>
+          <h3 id='quote'>"Crafting happiness, one slice at a time."<br/><span>-SLICE</span></h3>
+          <p id='paragraph'>Welcome to Slice Pizza Company, where every bite tells a delicious story. With a passion 
+            for crafting exceptional pizzas, we source the finest ingredients to ensure each slice bursts 
+            with flavor. Our menu offers classic combinations and unique gourmet creations to satisfy every 
+            craving. Handcrafted by skilled artisans, our pizzas are masterpieces that cater to all preferences, 
+            including gluten-free options. Exceptional service and community involvement are at the heart of our 
+            values. Join us for a memorable experience, where you can indulge in the joy of a perfect slice. At 
+            Slice Pizza Company, we're dedicated to crafting happiness, one bite at a time.</p>
+      </div>
+      
     </div>
   )
 }
